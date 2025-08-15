911f01ec678c06cb7b183efe18dd8e9e4a7119a1
Theprimagen <the.primeagen@aol.com>
2025-08-15 13:46:05 -0500

    Functioning game

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 from constants import *
+from players import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -9,12 +13,49 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable =  pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = [updatable, drawable]
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = [updatable]
+    Shot.containers = [shots, updatable, drawable]
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.collisions(asteroid):
+                print("Game over!")
+                exit()
+
+        for asteroid in asteroids:
+            for bullet in shots:
+                if bullet.collisions(asteroid):
+                    bullet.kill()
+                    asteroid.split()
+
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
+
+
         pygame.display.flip() #Refresh the screen
+        dt = clock.tick(60) / 1000
+    
 
 if __name__ == "__main__":
     main()
